7ed9f83a1104c4dabc67a832da547fae050de1f8
alakadeepak <alaka.krishnan04@gmail.com>
2025-11-03 21:25:36 +0000

    String operation committed

---
+++ Python_Basics.py
@@ -0,0 +1,19 @@
+user_prompt="Enter a todo:"
+
+# While Loop -Infinite - 1st condition
+# while True:
+#     todo=input(user_prompt)
+#     print(todo)
+
+# 2nd condition
+# while True: 
+#     todo=input(user_prompt)
+#     todos=[todo]#Overwrite the list in each loop
+#     print(todos)
+
+todos=[]
+while True:
+    todo=input(user_prompt)
+    todo.append()
+    todos.append(todo)
+    print(todos)
